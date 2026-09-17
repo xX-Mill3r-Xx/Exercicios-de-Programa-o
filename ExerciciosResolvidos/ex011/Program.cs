205c07be1b4e78dd96f32f0d31362bb031297b5d
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-09-17 16:45:02 -0300

    ✨ feat: add exercicies

---
+++ ExerciciosResolvidos/ex011/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Entre com o valor de N: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                int x = i * n;
+                Console.WriteLine($"{i} x {n} = {x}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
