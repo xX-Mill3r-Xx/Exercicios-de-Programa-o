1a7465e2e14f7a353ff09b6183a9a6c7ec0ce03f
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-09-18 17:29:35 -0300

    ✨ feat: add ex

---
+++ ExerciciosResolvidos/ex016/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int M, N;
+            int[,] A;
+
+            string[] s1 = Console.ReadLine().Split(' ');
+            M = int.Parse(s1[0]);
+            N = int.Parse(s1[1]);
+
+            A = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write($"{A[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
