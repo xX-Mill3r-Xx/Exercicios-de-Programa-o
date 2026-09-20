45f0a5c8fbb7df9f0012b30599602d4fe57fee13
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-09-20 08:29:02 -0300

    ✨ feat: add

---
+++ ExerciciosResolvidos/poo005/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace poo005
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, decimal preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public decimal ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados do produto: {Nome}, {Quantidade} unidades, Total: R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
