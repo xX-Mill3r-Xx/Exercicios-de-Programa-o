41a2faf416258e7acd17872a41a5e8a8dcd92cd3
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-09-20 16:50:25 -0300

    ✨ feat: add

---
+++ ExerciciosResolvidos/poo007/Entities/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace poo007.Entities
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, decimal preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, decimal preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public decimal ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados do produto: {Nome}, {Quantidade} unidades, Total: R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExerciciosResolvidos/poo007/Program.cs
@@ -0,0 +1,38 @@
+﻿using poo007.Entities;
+using System;
+using System.Globalization;
+
+namespace poo007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine($"Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.Preco = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(produto.ToString());
+
+            Console.Write("Digite a quantidade de produtos a serem adicionados ao estoque: ");
+            var quantidade = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(quantidade);
+
+            Console.WriteLine(produto.ToString());
+
+            Console.Write("Digite a quantidade de produtos a serem removidos do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(quantidade);
+
+            Console.WriteLine(produto.ToString());
+        }
+    }
+}
