0de37851d6b5e50b5c04ac622fb2b9e445389397
Miller M. Vasco <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-09-22 13:58:53 -0300

    ✨ feat: add exercicio de poo 008

---
+++ ExerciciosResolvidos/poo008/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo008
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Altura { get; set; }
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(string nome, int idade, double altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Altura = altura;
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade.ToString("F2", CultureInfo.InvariantCulture)} anos e {Altura}m de altura.");
+        }
+    }
+}
---
+++ ExerciciosResolvidos/poo008/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace poo008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Enunciado
+            /*Crie uma classe Pessoa com as propriedades Nome, Idade e Altura. Adicione:
+
+                Um construtor parametrizado
+
+                Um método Apresentar() que exibe no console: "Olá, meu nome é X, tenho Y anos e Z m de altura."
+
+                No Main, crie 3 pessoas diferentes e chame Apresentar() para cada uma.*/
+            #endregion
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+            int quantidadePessoas = 3;
+
+            for (int i = 0; i < quantidadePessoas; i++)
+            {
+                Console.Write($"Informe Nome, Idade e Altura da pessoa {i + 1}: ");
+                string[] pessoa = Console.ReadLine().Split(' ');
+                pessoas.Add(new Pessoa
+                {
+                    Nome = pessoa[0],
+                    Idade = int.Parse(pessoa[1]),
+                    Altura = double.Parse(pessoa[2], CultureInfo.InvariantCulture)
+                });
+            }
+
+            foreach (var pessoa in pessoas)
+            {
+                pessoa.Apresentar();
+            }
+        }
+    }
+}
