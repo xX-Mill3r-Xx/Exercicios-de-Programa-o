eea2fec76cdb3fd19fafe366f7870f3c43125109
Miller M. Vasco <millermartinsvasco1@gmail.com>
2026-09-22 22:19:59 -0300

    ✨ feat: add exercicio poo009

---
+++ ExerciciosResolvidos/poo009/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo009
+{
+    public class ContaBancaria
+    {
+        public string Titular { get; set; }
+        public decimal Saldo { get; private set; }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                throw new InvalidOperationException("Valor do deposito deve ser positivo.");
+            }
+
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new InvalidOperationException("O valor de saque não pode ser maior que o saldo.");
+            }
+
+            Saldo -= valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Titular: {Titular} | Saldo: {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExerciciosResolvidos/poo009/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace poo009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Enunciado
+            /*Crie uma classe ContaBancaria com:
+
+            Propriedades Titular (string) e Saldo (decimal, somente leitura externa — private set)
+
+            Métodos Depositar(decimal valor) e Sacar(decimal valor)
+
+            Regras:
+
+            Não permitir depósito ou saque de valor ≤ 0
+
+            Não permitir saque maior que o saldo
+
+            Exibir mensagem de sucesso ou erro em cada operação
+
+            Propriedade calculada Resumo que retorna: "Titular: X | Saldo: R$ Y"
+
+            Desafio: tente impedir que o Saldo seja alterado diretamente de fora da classe.*/
+            #endregion
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Entre com um valor para deposito inicial: ");
+            decimal valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            ContaBancaria conta = new ContaBancaria { Titular = titular };
+
+            conta.Depositar(valor);
+            Console.WriteLine($"Deposito inicial de {valor.ToString("F2", CultureInfo.InvariantCulture)} realizado com sucesso!");
+            Console.WriteLine(conta.ToString());
+
+            Console.Write("Entre com um valor para saque: ");
+            valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.Sacar(valor);
+            Console.WriteLine($"Saque de {valor.ToString("F2", CultureInfo.InvariantCulture)} realizado com sucesso!");
+            Console.WriteLine(conta.ToString());
+        }
+    }
+}
